da9a51674bc1360f520d524cd99a08bbe53a8497
Student Aman4580 <amanverma7124@gmail.com>
2024-07-15 00:12:49 +0530

    commit

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -5,6 +5,7 @@
 export const endpoints = {
   SIGNUP_API: 'http://localhost:4000/signup',
   LOGIN_API: 'http://localhost:4000/login',
+  CREATE_API: 'http://localhost:4000/create',
   // Add more endpoints as needed
 };
 
